a9a6caf8247e94c3d86b92c3e70bee1b283b6495
Alandlo1903 <56325950+alandlo1903@users.noreply.github.com>
2019-10-08 20:46:22 -0300

    mudança no programa
    alterei n1 para b1

--- intervalos_numericos.py
+++ intervalos_numericos.py
@@ -10,7 +10,7 @@ valor1< variavel < valor2
 
 #not(0<=n1<=10)or not(0<+n2<=10)or not(0<=n3<=10)
 
-n1 = float(input('n1:'))
+b1 = float(input('n1:'))
 n2 = float(input('n2:'))
 n3 = float(input('n3:'))
 if (n1<0 or n1>10)or(n2<0 or n2>10)or(n3<0 or n3>10):    
